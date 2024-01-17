4283fc248c6fdfe990fe9ad9e50960516f896725
Dylan Mayfield <dylanmayfield18@gmail.com>
2024-01-17 12:38:36 -0600

    added file structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+// import BucketList from './components/BucketList';
+
+function App() {
+  return (
+    <div className="React-Portfolio">
+      {/* <BucketList /> */}
+    </div>
+  );
+}
+
+export default App;
